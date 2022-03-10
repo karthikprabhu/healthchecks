c79954b8d65ae092d7987f3c6ffc7b3c6fc487b6
Karthik Prabhu <k.karthik.prabhu@gmail.com>
2022-03-11 00:44:11 +0530

    Integrate peek

--- hc/wsgi.py
+++ hc/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from opentelemetry.instrumentation.django import DjangoInstrumentor
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hc.settings")
 
 application = get_wsgi_application()
+
+DjangoInstrumentor().instrument()
